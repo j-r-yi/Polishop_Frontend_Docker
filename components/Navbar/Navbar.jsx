d19b2d819c883acfec9aaba1810eef4ea5f70b3a
Joshua Yi <jryi@scu.edu>
2024-09-01 23:52:14 +0800

    searchbar search result autopopulate

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 'use client';
 import NextLink from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useState, useEffect, useRef } from 'react';
 
 import { SearchIcon } from '@chakra-ui/icons';
 import {
@@ -7,9 +9,10 @@ import {
   InputGroup,
   InputRightAddon,
   IconButton,
-  Divider,
   Text,
   Image,
+  Divider,
+  Box,
 } from '@chakra-ui/react';
 
 import ListNavBar from './ListNavBar';
@@ -18,39 +21,78 @@ import ShoppingCartBtn from '../Cart/ShoppingCartBtn';
 import LocationModal from '../Navbar/LocationModal';
 import ProfileBtns from '../Account/ProfileBtns';
 
-const handleSearch = async function (term) {
+const handleSearch = async function (term, setSearchResults, setSearchData) {
   try {
     const res = await fetch(`http://127.0.0.1:8000/search/${term}`);
     if (!res.ok) {
       throw new Error('Invalid Search');
     }
     const data = await res.json();
+    setSearchResults(data);
+    setSearchData(data);
+    console.log(data);
+    return;
   } catch (error) {
     console.log('Cannot find item', error);
+    setSearchResults([]);
   }
 };
 
-import { useState } from 'react';
-
 export default function Navbar() {
+  const router = useRouter();
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchData, setSearchData] = useState([]);
+  const searchBarRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        searchBarRef.current &&
+        !searchBarRef.current.contains(event.target)
+      ) {
+        setSearchResults([]);
+      }
+    };
+
+    const handleClickInside = (event) => {
+      if (searchBarRef.current && searchBarRef.current.contains(event.target)) {
+        if (searchTerm != '') {
+          setSearchResults(searchData);
+        }
+      }
+    };
+    document.addEventListener('mousedown', handleClickOutside);
+    document.addEventListener('mousedown', handleClickInside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+      document.removeEventListener('mousedown', handleClickInside);
+    };
+  }, [searchData, searchTerm]);
 
   const handleSearchTermChange = function (e) {
     e.stopPropagation();
-    setSearchTerm(e.target.value);
+    const value = e.target.value;
+    setSearchTerm(value);
+    if (value != '') {
+      handleSearch(value, setSearchResults, setSearchData);
+    } else {
+      setSearchResults([]);
+    }
   };
 
   const handleKeyDown = function (e) {
     if (e.key === 'Enter') {
       console.log('enter key ');
       e.preventDefault();
-      handleSearch(searchTerm);
+      handleSearch(searchTerm, setSearchResults);
     }
   };
 
   return (
     <div className='flex flex-col justify-center items-center mx-auto bg-gradient-to-br from-blue-200 to-cyan-100'>
-      <div className='flex flex-col sm:flex-row items-center justify-evenly gap-10 pl-20 pr-20 w-full pb-3 pt-6'>
+      <div className='flex flex-col lg:flex-row items-center justify-evenly gap-10 pl-20 pr-20 w-full pb-3 pt-6'>
         <NextLink href='/' passHref>
           <div className='flex flex-row items-center gap-2 mr-10'>
             <Image src='/icon_copy.png' boxSize='50px' />
@@ -65,31 +107,68 @@ export default function Navbar() {
           </div>
         </NextLink>
 
-        <InputGroup size='md'>
-          <Input
-            placeholder='Search for items...'
-            width='90%'
-            variant='filled'
-            onChange={handleSearchTermChange}
-            onKeyDown={handleKeyDown}
-            bg='gray.100'
-            _focus={{ backgroundColor: 'gray.100' }}
-          />
-
-          <InputRightAddon width='4.5rem'>
-            <IconButton
-              colorScheme='white'
-              color='black'
-              aria-label='Search Items'
-              icon={<SearchIcon />}
-              w='full'
-              border='hidden'
-              onClick={() => {
-                handleSearch(searchTerm);
-              }}
+        <div className='w-full relative' ref={searchBarRef}>
+          <InputGroup size='md' ref={searchBarRef}>
+            <Input
+              placeholder='Search for items...'
+              width='90%'
+              variant='filled'
+              onChange={handleSearchTermChange}
+              onKeyDown={handleKeyDown}
+              bg='gray.100'
+              _focus={{ backgroundColor: 'gray.100' }}
             />
-          </InputRightAddon>
-        </InputGroup>
+
+            <InputRightAddon width='4.5rem'>
+              <IconButton
+                colorScheme='white'
+                color='black'
+                aria-label='Search Items'
+                icon={<SearchIcon />}
+                w='full'
+                border='hidden'
+                onClick={() => {
+                  handleSearch(searchTerm, setSearchResults);
+                }}
+              />
+            </InputRightAddon>
+          </InputGroup>
+
+          {searchResults.length > 0 ? (
+            <Box
+              position='absolute'
+              // top='100%'
+              width='90%'
+              bg='white'
+              zIndex='1000'
+              boxShadow='md'
+              p={4}
+              mt={2}
+              borderRadius='md'
+            >
+              <div className='flex flex-col'>
+                {searchResults.map((curr, idx) => (
+                  <div
+                    key={idx}
+                    className='flex flex-row items-center gap-10 w-full justify-start'
+                    onClick={() => {
+                      setSearchResults([]);
+                      setSearchTerm('');
+                      router.push(`/products/${curr.productId}`);
+                    }}
+                  >
+                    <Image src={curr.img} boxSize='50px' alt={curr.name} />
+                    <Text>{curr.productname}</Text>
+                    <Text>${curr.price}</Text>
+                    {/* <Divider /> */}
+                  </div>
+                ))}
+              </div>
+            </Box>
+          ) : (
+            <></>
+          )}
+        </div>
 
         <ProfileBtns></ProfileBtns>
         <ShoppingCartBtn></ShoppingCartBtn>
