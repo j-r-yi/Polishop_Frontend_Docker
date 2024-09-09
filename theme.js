5f4e8a5f2befd2c1b754d3801ff0337166fd1ad7
Joshua Yi <jryi@scu.edu>
2024-09-09 11:51:14 -0700

    temporary removal of montserrat

--- theme.js
+++ theme.js
@@ -1,8 +1,9 @@
-// "use client"
 import { extendTheme } from '@chakra-ui/react';
-import '@fontsource-variable/montserrat';
-// import '@fontsource/montserrat';
+// import '@fontsource-variable/montserrat';
+
 // import '@fontsource/montserrat/variable.css';
+// import '@fontsource/montserrat/400.css'; // Regular Montserrat font
+// import '@fontsource/montserrat/700.css'; // Bold Montserrat font
 
 export const theme = extendTheme({
   colors: {
